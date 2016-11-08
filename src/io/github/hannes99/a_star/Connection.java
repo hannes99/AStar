844683084a46fc6f9e413bf49e09fcb8cd7cca0f
Robert Obkircher <robert.obkircher@gmail.com>
2016-11-08 20:38:30 +0100

    connection und node dokumentiert
    node:
    position und user data zu getun

--- src/io/github/hannes99/a_star/Connection.java
+++ src/io/github/hannes99/a_star/Connection.java
@@ -1,29 +1,51 @@
 package io.github.hannes99.a_star;
 
 /**
- * Created by robert on 11/8/16.
+ * Represents a connection to another Node
  */
 public class Connection {
     private Node node;
     private double value;
 
+    /**
+     * Creates a new connection
+     *
+     * @param node  The connected node
+     * @param value The cost of this connection
+     */
     public Connection(Node node, double value) {
         setNode(node);
         setValue(value);
     }
 
+    /**
+     *
+     * @return The cost of this connection
+     */
     public double getValue() {
         return value;
     }
 
+    /**
+     *
+     * @param value The cost of this connection
+     */
     public void setValue(double value) {
         this.value = value;
     }
 
+    /**
+     *
+     * @return The connected node
+     */
     public Node getNode() {
         return node;
     }
 
+    /**
+     *
+     * @param node The connected node
+     */
     public void setNode(Node node) {
         this.node = node;
     }
--- src/io/github/hannes99/a_star/Node.java
+++ src/io/github/hannes99/a_star/Node.java
@@ -3,36 +3,67 @@ package io.github.hannes99.a_star;
 import java.util.ArrayList;
 
 /**
- * Created by robert on 11/8/16.
+ * A node with connections to other nodes.
  */
 public class Node {
-    private double g, h;
-    private Node predecessor;
+    // Extra
+    Object extra; // Can be used to store data
+    // Position
+    private double x, y;
+    // AStar
+    private double g, h; // G and H function values
+    private Node predecessor; // To store the shortest (known) way to start
     private ArrayList<Connection> connections = new ArrayList<Connection>();
 
+    /**
+     * Creates a new node
+     *
+     * @param x The X coordinate
+     * @param y The Y coordinate
+     */
+    public Node(int x, int y) {
+        setPosition(x, y);
+    }
+
+    /**
+     * @return G
+     */
     public double getG() {
         return g;
     }
 
+    /**
+     *
+     * @param g New value for G
+     */
     public void setG(double g) {
         this.g = g;
     }
 
+    /**
+     * Sets the Position
+     *
+     * @param x The X coordinate
+     * @param y The Y coordinate
+     */
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return F
+     */
     public double getF() {
         return getG() + getH();
     }
 
+    /**
+     *
+     * @return H
+     */
     public double getH() {
         return h;
-    }
-
-    /**
-     * Sets H to a specific Value
-     *
-     * @param value New value vor H
-     */
-    public void setH(double value) {
-        h = value;
     }
 
     /**
@@ -41,17 +72,72 @@ public class Node {
      * @param target The target node
      */
     public void setH(Node target) {
-        // TODO setH(distance);
+        double deltaX = getX() - target.getX();
+        double deltaY = getY() - target.getY();
+        setH(Math.sqrt(deltaX * deltaX + deltaY * deltaY));
     }
 
+    /**
+     *
+     * @param value New value vor H
+     */
+    public void setH(double value) {
+        h = value;
+    }
+
+    /**
+     *
+     * @return X coordinate
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     *
+     * @return Y coordinate
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * @return User data
+     */
+    public Object getExtra() {
+        return extra;
+    }
+
+    /**
+     * Can be used to store data related to this node.
+     *
+     * @param extra User data
+     */
+    public void setExtra(Object extra) {
+        this.extra = extra;
+    }
+
+    /**
+     * Stores the shortest known way to start.
+     *
+     * @return The previous node
+     */
     public Node getPredecessor() {
         return predecessor;
     }
 
+    /**
+     *
+     * @param previousNode Better way to start
+     */
     public void setPredecessor(Node previousNode) {
         this.predecessor = previousNode;
     }
 
+    /**
+     *
+     * @return All connections from this node
+     */
     public ArrayList<Connection> getConnections() {
         return connections;
     }
